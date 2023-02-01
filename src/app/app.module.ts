33e18b716faf086971b832b0e94c1aa6ad1f7a84
jpelz <jennifer.pelz@qudosoft.de>
2023-02-01 17:03:57 +0100

    🚮 Delete unnecessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GeneratorComponent } from '../generator/generator.component';
+import { GeneratorComponent } from './components/generator/generator.component';
 
 @NgModule({
   declarations: [
